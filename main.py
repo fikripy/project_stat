7ee5759477e0ff703bf40dc85d23640d1633e3b8
fikripy <fikrisaintis96@gmail.com>
2021-12-15 17:38:28 -0800

    bingung di anova yang 2 jalur nya

--- main.py
+++ main.py
@@ -7,21 +7,55 @@ import streamlit as st
 from scipy import stats
 import plotly_express as px, researchpy as rp
 from itertools import combinations as C
+from statsmodels.stats.contingency_tables import mcnemar
 
 tempat_upload = st.file_uploader('**Unggah Data**')
 if tempat_upload is not None:
     df = pd.read_csv(tempat_upload)
-    c1, c2 = st.columns(2)
-    with c1:
-        st.dataframe(df)
-    with c2:
-        fig = px.box(df)
-        st.plotly_chart(fig)
+    df_ori = df.copy()
+#Mengecek Homogenitas Data
+    def periksa_homogenitas(df):
+        df_copy = df.copy()
+        if df.shape[1] > 1:
+            cols = df.columns
+            data = [df[i].values for i in cols]
+            p_levene = stats.levene(*data)[1]
+            if p_levene < .05:
+                #print('log trans')
+                df = np.log(df + (-df.min().min()) + 1)
+                data_trans = [df[i].values for i in cols]
+                p_levene2 = stats.levene(*data_trans)[1]
+                if p_levene2 < .05:
+                    #print('sqrt trans')
+                    df = np.sqrt(df_copy + (-df_copy.min().min()) + 1)
+                else: pass
+            else: pass
+        else: pass
+        return df
+    df = periksa_homogenitas(df)
+    def display(df):
+        if df.equals(df_ori):
+            c1, c2 = st.columns(2)
+            with c1:
+                st.dataframe(df_ori)
+            with c2:
+                fig = px.box(df_ori)
+                st.plotly_chart(fig)
+        else:
+            c1, c2, c3 = st.columns(3)
+            with c1:
+                st.dataframe(df_ori)
+            with c2:
+                st.dataframe(df)
+            with c3:
+                fig = px.box(df)
+                st.plotly_chart(fig)
+    display(df)
     if df.shape[1] == 1:
         uji_1_grup = st.sidebar.selectbox(
             'Pilih Uji', ['Uji T Satu Sampel','Uji Chi Square 1 Sampel']
         )
-        if uji_1_grup == 'One Sample T Test':
+        if uji_1_grup == 'Uji T Satu Sampel':
             q1, q2, q3 = df.iloc[:, 0].quantile([.25, .5, .75])
             iqr = q3 - q1
             batas_bawah = q1 - (1.5*iqr)
@@ -34,8 +68,9 @@ if tempat_upload is not None:
             chi_stats, p_value_chi = stats.chisquare(df.iloc[:, 0])
             st.write('Dengan Uji Chi Square didapatkan P value = {:.2f}'.format(p_value_chi))
     elif df.shape[1] == 2:
+        #t_or_aov = st.sidebar.selectbox('Jumlah Grup', ['2', '>2'])
         uji_2_grup = st.sidebar.selectbox(
-            'Pilih Uji 2 Grup', ['Uji T', 'Uji Chi Square']
+            'Pilih Uji 2 Grup', ['Uji T', 'Uji Chi Square', '>2 grup']
         )
         if uji_2_grup == 'Uji T':
             Hubungan = st.sidebar.radio(
@@ -63,7 +98,9 @@ if tempat_upload is not None:
             chi, p, dof, ex = stats.chi2_contingency(df.iloc[:, 0], df.iloc[:, 1])
             st.write('nilai p value nya = {:.2f}'.format(p))
     else:
-        st.write('uji 3 grup')
+        uji_3_grup = st.sidebar.selectbox(
+            'Uji Statistik', ['Anova','Chi Squared']
+        )
         def check_p_value(df):
             cols = df.columns
             data = [df[i].values for i in cols]
@@ -72,12 +109,25 @@ if tempat_upload is not None:
             p_value_arr = np.array(p_value_list)
             return data, p_value_arr
         data, p_value_arr = check_p_value(df)[0], check_p_value(df)[1]
-        if (p_value_arr < .05).any():
-            stats_kruskall, p_value_kruskal = stats.kruskal(*data)
-            st.write('Uji Kruskal dengan P Value = {:.2f}'.format(p_value_kruskal))
+        if uji_3_grup == 'Anova':
+            Hubungan = st.sidebar.selectbox(
+                'Hubungan', ['Berpasangan', 'Tidak Berpasangan']
+            )
+            if (p_value_arr < .05).any():
+                if Hubungan == 'Tidak Berpasangan':
+                    stats_kruskall, p_value_kruskal = stats.kruskal(*data)
+                    st.write('Uji Kruskal dengan P Value = {:.2f}'.format(p_value_kruskal))
+                else:
+                    stats_friedman, p_value_friedman = stats.friedmanchisquare(*data)
+                    st.write('Uji Friedman dengan P value = {:.2f}'.format(p_value_friedman))
+            else:
+                stats_anova, p_value_anova = stats.f_oneway(*data)
+                st.write('Uji Anova Satu Jalur dengan P Value {:.2f}'.format(p_value_anova))
+                
         else:
-            stats_anova, p_value_anova = stats.f_oneway(*data)
-            st.write('Uji Anova Satu Jalur dengan P Value {:.2f}'.format(p_value_anova))
+            chi, p, dof, ex = stats.chi2_contingency(*data)
+            st.write('nilai p value nya = {:.2f}'.format(p))
+
 else:
     st.stop()
 
